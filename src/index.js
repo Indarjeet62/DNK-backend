b8368e203d114be924d42ac5df971caefc3d9ed1
ABHINAV PRAKASH <abhinav84094@gmail.com>
2025-01-02 20:43:58 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
-const ServerConfig = require('./config/serverConfig')
+const ServerConfig = require('./config/serverConfig');
+const connectDB = require('./config/dbConfig');
 
 const app = express();
 
-app.listen(ServerConfig.PORT, ()=>{
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
+
+app.post('/ping', (req, res)=>{
+    console.log(req.body);
+    return res.json({message:"pong"})
+})
+
+
+app.listen(ServerConfig.PORT, async ()=>{
+    await connectDB();
     console.log(`Server started at ${ServerConfig.PORT}`);
 })
+
